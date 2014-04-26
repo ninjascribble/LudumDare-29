cc4f44e24984ee4f4a9e9ebe6c4f818c97ba1b54
ninjascribble <scott@ninjascript.com>
2014-04-26 13:12:59 -0700

    Using game timer for the moles because it is cool

--- game/prefabs/mole.js
+++ game/prefabs/mole.js
@@ -4,7 +4,7 @@ var Mole = function(game, x, y, frame) {
   Phaser.Sprite.call(this, game, x, y, 'mole', frame);
   this.game.add.existing(this);
   this.anchor.setTo(0.5, 0.5);
-  this.frequency = Math.random() * 600 + 800;
+  this.game.time.events.loop(Math.random() * 600 + 800, onTimerTick, this);
   this.lastUpdated = Date.now();
 };
 
@@ -12,13 +12,10 @@ Mole.prototype = Object.create(Phaser.Sprite.prototype);
 Mole.prototype.constructor = Mole;
 
 Mole.prototype.update = function() {
-
-  var now = Date.now();
-
-  if (now - this.lastUpdated > this.frequency) {
-    this.frame = (this.frame == 0) ? 1 : 0;
-    this.lastUpdated = now;
-  }
 };
+
+function onTimerTick() {
+    this.frame = (this.frame == 0) ? 1 : 0;
+}
 
 module.exports = Mole;
