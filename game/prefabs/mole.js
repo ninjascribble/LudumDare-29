c08ab37322e2670b5ae62fe85ce0e8585e97c7f5
ninjascribble <scott@ninjascript.com>
2014-04-26 11:44:28 -0700

    Moles in grids!

--- game/prefabs/mole.js
+++ game/prefabs/mole.js
@@ -2,15 +2,22 @@
 
 var Mole = function(game, x, y, frame) {
   Phaser.Sprite.call(this, game, x, y, 'mole', frame);
+  this.anchor.setTo(0.5, 0.5);
+  this.frequency = Math.random() * 600 + 800;
+  this.lastUpdated = Date.now();
 };
 
 Mole.prototype = Object.create(Phaser.Sprite.prototype);
 Mole.prototype.constructor = Mole;
 
 Mole.prototype.update = function() {
-  
-  // write your prefab's specific update code here
-  
+
+  var now = Date.now();
+
+  if (now - this.lastUpdated > this.frequency) {
+    this.frame = (this.frame == 0) ? 1 : 0;
+    this.lastUpdated = now;
+  }
 };
 
 module.exports = Mole;
--- game/states/play.js
+++ game/states/play.js
@@ -1,36 +1,29 @@
 var Mole = require('../prefabs/mole');
 
 'use strict';
-function Play() {}
+function Play() {
+  this.moles = [];
+  this.molesPerRow = 4;
+  this.molesPerColumn = 4;
+}
+
+function calculatePos(i, n, length) {
+  var result = (i / n) * length - (length / n * .5);
+  return result;
+}
+
 Play.prototype = {
 
   create: function() {
 
     this.game.physics.startSystem(Phaser.Physics.ARCADE);
 
-    this.mole1 = new Mole(this.game, this.game.width * .3, this.game.height * .3);
-    this.game.add.existing(this.mole1);
-
-    this.mole2 = new Mole(this.game, this.game.width * .6, this.game.height * .3);
-    this.game.add.existing(this.mole2);
-
-    this.mole3 = new Mole(this.game, this.game.width * .3, this.game.height * .6);
-    this.game.add.existing(this.mole3);
-
-    this.mole4 = new Mole(this.game, this.game.width * .6, this.game.height * .6);
-    this.game.add.existing(this.mole4);
-
-    
-    // this.sprite.inputEnabled = true;
-    
-    // this.game.physics.arcade.enable(this.sprite);
-
-    // this.sprite.body.collideWorldBounds = true;
-    // this.sprite.body.bounce.setTo(1,1);
-    // this.sprite.body.velocity.x = this.game.rnd.integerInRange(-500,500);
-    // this.sprite.body.velocity.y = this.game.rnd.integerInRange(-500,500);
-
-    // this.sprite.events.onInputDown.add(this.clickListener, this);
+    for (var i = 0, len = this.molesPerRow; i < len; i++) {
+      for (var j = 0, len = this.molesPerColumn; j < len; j++) {
+        this.moles.unshift(new Mole(this.game, calculatePos(i + 1, this.molesPerRow, this.game.width), calculatePos(j + 1, this.molesPerColumn, this.game.height)));
+        this.game.add.existing(this.moles[0]);
+      }
+    }
   },
   
   update: function() {
