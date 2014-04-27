effbe9d230af500bac261333f4201d888e6a8f30
Ben McEldowney <bmceldowney@gmail.com>
2014-04-27 00:56:22 -0700

    fixing up cyndi

--- game/states/preload.js
+++ game/states/preload.js
@@ -17,7 +17,7 @@ Preload.prototype = {
 
     // player
     //this.load.atlas('player', 'assets/player/playerOne.png', 'assets/player/playerOne.json');
-    this.load.spritesheet('cyndi', 'assets/player/walk-left-280.png', 152, 280);
+    this.load.spritesheet('cyndi', 'assets/player/walk-right-140.png', 76, 140);
 
     // items
     this.load.spritesheet('bomb', 'assets/items/bomb.png', 70, 70);
