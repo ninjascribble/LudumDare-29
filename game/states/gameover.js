fbfd5f45783f99a5b489275b782912e6edff8ab5
ninjascribble <scott@ninjascript.com>
2014-04-28 12:44:05 -0700

    repositioning gameover text

--- game/states/gameover.js
+++ game/states/gameover.js
@@ -24,15 +24,15 @@ GameOver.prototype = {
     //this.scoreText.updateTransform();
     //this.scoreText.x = this.game.width / 2 - this.scoreText.textWidth / 2;
 
-    this.titleText = this.game.add.bitmapText(this.game.world.centerX, 200, '8bit-light', 'The Moles Win', 48);
+    this.titleText = this.game.add.bitmapText(this.game.world.centerX, 225, '8bit-light', 'The Moles Win', 48);
     this.titleText.updateTransform();
     this.titleText.x = this.game.width / 2 - this.titleText.textWidth / 2;
 
-    this.messageText = this.game.add.bitmapText(this.game.world.centerX, 257.5, '8bit-light', message, 24);
+    this.messageText = this.game.add.bitmapText(this.game.world.centerX, 282.5, '8bit-light', message, 24);
     this.messageText.updateTransform();
     this.messageText.x = this.game.width / 2 - this.messageText.textWidth / 2;
 
-    this.moveText = this.game.add.bitmapText(this.game.world.centerX, 300, '8bit-light', 'Click to play again', 16);
+    this.moveText = this.game.add.bitmapText(this.game.world.centerX, 325, '8bit-light', 'Click to play again', 16);
     this.moveText.updateTransform();
     this.moveText.x = this.game.width / 2 - this.moveText.textWidth / 2;
   },
