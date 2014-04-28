27d64fb6b653d2077cacfb0fd89f83b9992df5b8
ninjascribble <scott@ninjascript.com>
2014-04-27 22:37:21 -0700

    Fixing SpriteBroker AMD module compatibility

--- game/services/spriteBroker.js
+++ game/services/spriteBroker.js
@@ -6,90 +6,97 @@ var Mole = require('../prefabs/mole');
 var SpittingMole = require('../prefabs/spittingMole');
 var Bomb = require('../prefabs/bomb');
 var Types = {
-	'player': Player,
-	'mole': Mole,
-	'spitting-mole': SpittingMole,
-	'bomb': Bomb
+    'player': Player,
+    'mole': Mole,
+    'spitting-mole': SpittingMole,
+    'bomb': Bomb
 };
 
 var SpriteBroker = function(game) {
-	this.pools = {};
-	this.game = game;
+    this.pools = {};
+    this.game = game;
 };
 
 SpriteBroker.prototype = {
 
-	get: function(type, criteria) {
+    get: function(type, criteria) {
 
-		checkType(type);
+        checkType(type);
 
-		if (!this.pools[type]) {
-			this.pools[type] = new Pool(Types[type], [this.game, -20000, -20000]);
-		}
+        if (!this.pools[type]) {
+            this.pools[type] = new Pool(Types[type], [this.game, -20000, -20000]);
+        }
 
-		return this.pools[type].get(criteria);
-	},
+        return this.pools[type].get(criteria);
+    },
 
-	count: function(type, criteria) {
-		checkType(type);
-		return (this.pools[type]) ? this.pools[type].getAll(criteria).length : 0;
-	},
+    count: function(type, criteria) {
+        checkType(type);
+        return (this.pools[type]) ? this.pools[type].getAll(criteria).length : 0;
+    },
 
-	forEach: function(type, callback, args, criteria) {
+    forEach: function(type, callback, args, criteria) {
 
-		checkType(type);
+        checkType(type);
 
-		if (this.pools[type]) {
-			this.pools[type].applyAll(callback, args, criteria);
-		}
-	},
+        if (this.pools[type]) {
+            this.pools[type].applyAll(callback, args, criteria);
+        }
+    },
 
-	callEach: function(type, method, args, criteria) {
+    callEach: function(type, method, args, criteria) {
 
-		checkType(type);
+        checkType(type);
 
-		if (this.pools[type]) {
-			this.pools[type].callAll(method, args, criteria);
-		}
-	},
+        if (this.pools[type]) {
+            this.pools[type].callAll(method, args, criteria);
+        }
+    },
 
-	countLiving: function(type) {
-		return this.count(type, { alive: true });
-	},
+    destroy: function(type) {
+        this.callEach(type, 'destroy', [true]);
+        this.pools[type].length = 0;
+    },
 
-	countDead: function(type) {
-		return this.count(type, { alive: false });
-	},
+    countLiving: function(type) {
+        return this.count(type, { alive: true });
+    },
 
-	countExists: function(type, value) {
-		return this.count(type, { exists: Boolean(value) });
-	},
+    countDead: function(type) {
+        return this.count(type, { alive: false });
+    },
 
-	getFirstAlive: function(type) {
-		return this.get(type, { alive: true });
-	},
+    countExists: function(type, value) {
+        return this.count(type, { exists: Boolean(value) });
+    },
 
-	getFirstDead: function(type) {
-		return this.get(type, { alive: false });
-	},
+    getFirstAlive: function(type) {
+        return this.get(type, { alive: true });
+    },
 
-	getFirstExists: function(type, value) {
-		return this.get(type, { exists: Boolean(value) });
-	}
+    getFirstDead: function(type) {
+        return this.get(type, { alive: false });
+    },
 
-	forEachAlive: function(type, callback, args) {
-		this.forEach(type, callback, args, { alive: true });
-	},
+    getFirstExists: function(type, value) {
+        return this.get(type, { exists: Boolean(value) });
+    }
 
-	forEachDead: function(type, callback, args) {
-		this.forEach(type, callback, args, { alive: false });
-	},
+    forEachAlive: function(type, callback, args) {
+        this.forEach(type, callback, args, { alive: true });
+    },
 
-	forEachExists: function(type, callback, value, args) {
-		this.forEach(type, callback, args, { exists: Boolean(value) });
-	}
+    forEachDead: function(type, callback, args) {
+        this.forEach(type, callback, args, { alive: false });
+    },
+
+    forEachExists: function(type, callback, value, args) {
+        this.forEach(type, callback, args, { exists: Boolean(value) });
+    }
 };
 
 function checkType(type) {
-	if (!Types[type]) throw new Error('Type doesn\'t exist: $1'.replace('$1', type));
+    if (!Types[type]) throw new Error('Type doesn\'t exist: $1'.replace('$1', type));
 }
+
+module.exports = SpriteBroker;
